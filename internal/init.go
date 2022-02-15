cc823dd04bbf033b1d4fa50edaf814a809afcc9a
wangxingyue <xingyue.wang@supos.com>
2022-02-15 14:36:48 +0800

    feat: format response text and add version info

--- internal/init.go
+++ internal/init.go
@@ -1,11 +1,10 @@
 package internal
 
-import (
-	"worldmaomao/web-hellword/internal/rest"
-)
+import "fmt"
 
 func Initial() {
 
+	fmt.Println("hello world")
 	// 启动web server
-	rest.NewServer().Start()
+	// rest.NewServer().Start()
 }
